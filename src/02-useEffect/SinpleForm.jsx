6fc359c47d0d5d83d96848a99e4b315d3ef2af4b
LuisJavier55 <luis.jav.More@hotmail.com>
2024-06-24 17:32:28 -0600

    cambio

---
+++ src/02-useEffect/SinpleForm.jsx
@@ -0,0 +1,21 @@
+
+
+export const SinpleForm = () => {
+  return (
+    <>
+    <h1>Simple Form</h1>
+
+    <hr />
+
+
+    <input type="text"
+    className="form-control"
+    placeholder="Username"
+    name="username" />
+    </>
+  )
+}
+
+
+
+
