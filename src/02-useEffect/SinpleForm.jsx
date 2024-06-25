db81ff9b5b81d3111b90a4356894cd2f4ca1b605
LuisJavier55 <luis.jav.More@hotmail.com>
2024-06-25 17:38:07 -0600

    commit simpleform

--- src/02-useEffect/SinpleForm.jsx
+++ src/02-useEffect/SinpleForm.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react"
 
 
 export const SinpleForm = () => {
+
+  const [estado, setEstado] = useState({
+    name: "username",
+    email: "Luis@google.com",
+  })
+//desestructurar el estado 
+  const {name, email} = estado
+
+//funcion del evento onchage la cual envia el evento y ese evento desestructura un 
+  const onEventEstado = ({target}) =>{
+    console.log(target);
+    console.log(target.value);
+
+
+  }
+
   return (
     <>
     <h1>Simple Form</h1>
@@ -11,7 +28,19 @@ export const SinpleForm = () => {
     <input type="text"
     className="form-control"
     placeholder="Username"
-    name="username" />
+    name="username"
+    value={name}
+    //funcion que se envia como prop
+    onChange={onEventEstado} />
+    
+    
+    <input type="email"
+    className="form-control mt-2"
+    placeholder="Luis@hotmail.com"
+    name="email"
+    value={email}
+    onChange={onEventEstado} />
+    
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CounterCustomHook } from './01-useState/CounterCustomHook'
+// import { CounterCustomHook } from './01-useState/CounterCustomHook'
+import { SinpleForm } from './02-useEffect/SinpleForm'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterCustomHook />
+    {/* <CounterCustomHook /> */}
+    <SinpleForm />
   </React.StrictMode>,
 )
