e7eab102349b51dfb901037eb19e8f46596d9e4a
LuisJavier55 <luis.jav.More@hotmail.com>
2024-07-01 17:28:46 -0600

    arreglo de error con  el objeto useState

--- src/02-useEffect/SinpleForm.jsx
+++ src/02-useEffect/SinpleForm.jsx
@@ -4,18 +4,28 @@ import { useState } from "react"
 export const SinpleForm = () => {
 
   const [estado, setEstado] = useState({
-    name: "username",
-    email: "Luis@google.com",
+    username: "",
+    email: "",
   })
 //desestructurar el estado 
-  const {name, email} = estado
+  const {username, email} = estado
 
 //funcion del evento onchage la cual envia el evento y ese evento desestructura un 
-  const onEventEstado = ({target}) =>{
-    console.log(target);
-    console.log(target.value);
+  const onEventEstado = ({ target }) =>{
+
+    console.log(target);// evento mas target, el cual nos trae el input 
+
+    const {name, value} = target; //desestructuracion de target trayendo los atrubutos de nuestra etiqueta
+
+    setEstado({ //funcion en el cual trae nuestro estado actual 
+      ...estado, //spread operation trae todo el contenido de nuestro objeto 
+      [name]:value, // si hay un cambio, en las etiquetas=atributos se cambia tanto username o email y valores de name y value se integran   
+    })
+//esto nos ayuda a hacer un cambio cuando nuestro estado cambie(inputo), con los OnEventEstado y lo integra a un nuevo objeto.
+//el espread operation que tenemos es dinamico, si hay mas valorezs, se integraran por defecto en nuestro cambio de estado.
 
 
+   
   }
 
   return (
@@ -27,16 +37,16 @@ export const SinpleForm = () => {
 
     <input type="text"
     className="form-control"
-    placeholder="username"
-    name="username"
-    value={name}
+    placeholder="Username"
+    name="username" // el username esta vinculado con el de nuestro objeto useState = el cual si nosotros ponemos mal uno el input no funcionara
+    value={username}
     //funcion que se envia como prop
     onChange={onEventEstado} />
     
     
     <input type="email"
     className="form-control mt-2"
-    placeholder="Luis@hotmail.com"
+    placeholder="correo@"
     name="email"
     value={email}
     onChange={onEventEstado} />
