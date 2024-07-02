c664d44d2506237c6d1022589c7cc5e7b9b104a3
LuisJavier55 <luis.jav.More@hotmail.com>
2024-07-02 16:56:56 -0600

    cambio de commit ahora si bien

--- src/02-useEffect/SinpleForm.jsx
+++ src/02-useEffect/SinpleForm.jsx
@@ -4,19 +4,15 @@ import { useState } from "react"
 export const SinpleForm = () => {
 
   const [estado, setEstado] = useState({
-<<<<<<< HEAD
     username: "",
     email: "",
-=======
-    name: "Luis",
-    email: "Luis@google.com",
->>>>>>> a4b059eea2abef7e68587c693fc73e56aa9d393a
+    
+
   })
 //desestructurar el estado 
   const {username, email} = estado
 
 //funcion del evento onchage la cual envia el evento y ese evento desestructura un 
-<<<<<<< HEAD
   const onEventEstado = ({ target }) =>{
 
     console.log(target);// evento mas target, el cual nos trae el input 
@@ -27,21 +23,9 @@ export const SinpleForm = () => {
       ...estado, //spread operation trae todo el contenido de nuestro objeto 
       [name]:value, // si hay un cambio, en las etiquetas=atributos se cambia tanto username o email y valores de name y value se integran   
     })
+  }
 //esto nos ayuda a hacer un cambio cuando nuestro estado cambie(inputo), con los OnEventEstado y lo integra a un nuevo objeto.
 //el espread operation que tenemos es dinamico, si hay mas valorezs, se integraran por defecto en nuestro cambio de estado.
-
-=======
-  const onEventEstado = ({target}) =>{
-    const {name, value} = target; //desestructuramos en tagert con el fin de extraer su numbre y valor del evento onchange 
-    
-    setEstado({
-      ...estado,
-      [ name ]:value
-    });
->>>>>>> a4b059eea2abef7e68587c693fc73e56aa9d393a
-
-   
-  }
 
   return (
     <>
@@ -53,13 +37,8 @@ export const SinpleForm = () => {
     <input type="text"
     className="form-control"
     placeholder="Username"
-<<<<<<< HEAD
-    name="username" // el username esta vinculado con el de nuestro objeto useState = el cual si nosotros ponemos mal uno el input no funcionara
+    name="username"// el username esta vinculado con el de nuestro objeto useState = el cual si nosotros ponemos mal uno el input no funcionara
     value={username}
-=======
-    name="name"
-    value={name}
->>>>>>> a4b059eea2abef7e68587c693fc73e56aa9d393a
     //funcion que se envia como prop
     onChange={onEventEstado} />
     
@@ -73,8 +52,8 @@ export const SinpleForm = () => {
     
     </>
   )
+ 
 }
-
 
 
 
